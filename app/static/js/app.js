8e0bc059e658ebbb53825eaf01d1c1a8eec006c2
sheymen <sheymen@gmail.com>
2017-06-15 18:35:07 -0400

    first version

---
+++ app/static/js/app.js
@@ -0,0 +1,110 @@
+(function($){
+
+	var Schedule = {
+		variables: {
+			step: 30, //minutes
+			step_height: 50, // Px
+			start: '2016-05-16 00:00:00',
+		},
+		initialize: function() {
+			// Render week headers
+			for (var i = 0;i<7;i++) {
+				var day = moment(this.variables.start).add(i,'days');
+				$(".week-header .column-day:eq("+i+")").html(day.format("ddd DD"));
+			}
+			// Render schedule
+			for (var i = 0;i<24*60/this.variables.step;i++) {
+				var time = moment(this.variables.start).add(i*this.variables.step,'minutes');
+				var step_element = $("<div class='step'>"+time.format("HH:mm")+"</div>");
+				step_element.css({height: this.variables.step_height+"px"});
+				$(".steps").append(step_element);
+			}
+			$(".schedule-container").css({
+				height: $(".steps").height()+"px"
+			});
+
+			// Fetch data
+
+			$.getJSON("../static/js/data.json",function(r){
+				console.log("Process me");
+				// console.log(r);
+				var hours = r['data']
+				var sorted = _.sortBy(hours, "start_at")
+				n = sorted.length
+				lastHour = ""
+				numObj = 0
+				count_margin = 0
+				for(var i = 0;i<n;i++){
+
+					dayHour = String(sorted[i]['start_at'].substr(8,3))
+					start_at = String(sorted[i]['start_at'].substr(10,11))
+					due_at = String(sorted[i]['due_at'].substr(10,11))
+					if(i==0){
+						lastHour = due_at
+					}
+
+					iniHours = start_at.substr(0,3);
+					finHours = due_at.substr(0,3);
+					iniMinutes = start_at.substr(4,2);
+					finMinutes = due_at.substr(4,2);
+
+					longHour =  parseInt(finHours) - parseInt(iniHours)
+					longMinutes =  parseInt(finMinutes) - parseInt(iniMinutes)
+
+					if(longHour>0){
+						totalPixelesH= longHour* 100}
+					else{totalPixelesH= 0}
+
+					if(longMinutes>0){
+						totalPixelesM= 50}
+					else{totalPixelesM= 0}
+					totalPixeles = totalPixelesH +totalPixelesM
+
+					marginTop = parseInt(iniHours) * 100
+					if(iniMinutes > 0){
+						marginTop = marginTop+50
+					}
+					if(start_at >= lastHour || numObj > 2)
+					{
+						if(count_margin > 1){
+							var hour_element = $("<div class='hour' style='width:50px ;height: "+totalPixeles+"px; margin-top:"+marginTop+"px; margin-left: -50px;'></div>");
+							count_margin = 0
+						}else{
+							var hour_element = $("<div class='hour' style='width:50px ;height: "+totalPixeles+"px; margin-top:"+marginTop+"px; margin-left: -100px;'></div>");
+							count_margin = count_margin +1
+						}
+
+						numObj = 0
+					}else{
+						var hour_element = $("<div class='hour' style='width:50px ;height: "+totalPixeles+"px; margin-top:"+marginTop+"px;'></div>");
+						numObj = numObj +1
+						count_margin = 0
+					}
+					lastHour = due_at
+
+					// obtener dia y ordenar en base a esto
+					for (var x = 1;x<7;x++) {
+						var day = moment(Schedule.start).add(x,'days');
+						dayM = day.format("DD");
+						if(parseInt(dayHour) == parseInt(dayM)){
+								colum = x-1
+								$(".schedule-container .column-day:eq("+colum+")").append(hour_element);
+						}
+
+					}
+
+				}
+
+				// console.log($( ".column-day").contents().find( "Mon 16" ));
+
+				// $( ".column-day").contents().find( "Mon 16" ).append( "<p>Test</p>" );
+
+			}).error(function(err){
+				console.log(err);
+			})
+		}
+	}
+
+	Schedule.initialize();
+
+})(jQuery);
---
+++ readme_test.md
@@ -0,0 +1,30 @@
+# Dentalink Code Challenge
+
+This is a simple challenge where you can show all your skills. There are no rules, it's absolute open to however you want to solve it.
+
+We also want to understand how talented are you figuring out the best possible solutions for a single problem.
+
+## Schedule challenge
+
+Index.html is a simple calendar structure (like Google Calendar) with no events rendered.
+
+Given /js/data.json, which is already requested via AJAX in /js/app.js, render items on the corresponding day, in a way that overlapping events are re-arranged in columns.
+
+An expected output can be found in /img/expected.png.
+
+## How to run
+
+Use any HTTP server to run the index.html.
+
+**Python**
+
+python -M SimpleHTTPServer
+
+**PHP**
+
+php -S localhost:8000
+
+You can also use apache or nginx. Just serve static content.
+
+## Tips
+The key is not on the CSS. Each event can be an absolute positioned div.
